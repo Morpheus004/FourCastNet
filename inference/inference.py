fc4a11cae7faffb8901e56c23e805277639c48d9
Morpheus004 <134723366+Morpheus004@users.noreply.github.com>
2025-01-22 00:14:51 +0530

    Ch -> checkpoint

--- inference/inference.py
+++ inference/inference.py
@@ -88,7 +88,7 @@ def load_model(model, params, checkpoint_file):
     model.zero_grad()
     checkpoint_fname = checkpoint_file
     checkpoint = torch.load(checkpoint_fname)
-    print("Printing contents of checkpoint ",dir(ch))
+    print("Printing contents of checkpoint ",dir(checkpoint))
     try:
         new_state_dict = OrderedDict()
         for key, val in checkpoint['model_state'].items():
