4a65c4b8d80e6bf898bda1f493232cff981fd55c
Morpheus004 <134723366+Morpheus004@users.noreply.github.com>
2025-01-21 08:11:12 +0530

    Remaining changes for pl file

--- copernicus/get_data_pl_short_length.py
+++ copernicus/get_data_pl_short_length.py
@@ -1,32 +1,68 @@
+# import cdsapi
+#
+# c = cdsapi.Client()
+#
+# c.retrieve(
+#     'reanalysis-era5-pressure-levels',
+#     {
+#         'product_type': 'reanalysis',
+#         'format': 'netcdf',
+#         'variable': [
+#             'geopotential', 'relative_humidity', 'temperature',
+#             'u_component_of_wind', 'v_component_of_wind',
+#         ],
+#         'pressure_level': [
+#             '50', '500', '850',
+#             '1000',
+#         ],
+#         'year': '2021',
+#         'month': '10',
+#         'day': [
+#             '19', '20', '21',
+#             '22', '23', '24',
+#             '25', '26', '27',
+#             '28', '29', '30',
+#             '31',
+#         ],
+#         'time': [
+#             '00:00', '06:00', '12:00',
+#             '18:00',
+#         ],
+#     },
+#     '/project/projectdirs/dasrepo/ERA5/oct_2021_19_31_pl.nc')
+
 import cdsapi
 
-c = cdsapi.Client()
+dataset = "reanalysis-era5-pressure-levels"
+request = {
+    "product_type": ["reanalysis"],
+    "variable": [
+        "geopotential",
+        "relative_humidity",
+        "temperature",
+        "u_component_of_wind",
+        "v_component_of_wind"
+    ],
+    "year": ["2022"],
+    "month": ["01"],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13"
+    ],
+    "time": [
+        "00:00", "06:00", "12:00",
+        "18:00"
+    ],
+    "pressure_level": [
+        "50", "500", "850",
+        "1000"
+    ],
+    "data_format": "netcdf",
+    "download_format": "unarchived"
+}
 
-c.retrieve(
-    'reanalysis-era5-pressure-levels',
-    {
-        'product_type': 'reanalysis',
-        'format': 'netcdf',
-        'variable': [
-            'geopotential', 'relative_humidity', 'temperature',
-            'u_component_of_wind', 'v_component_of_wind',
-        ],
-        'pressure_level': [
-            '50', '500', '850',
-            '1000',
-        ],
-        'year': '2021',
-        'month': '10',
-        'day': [
-            '19', '20', '21',
-            '22', '23', '24',
-            '25', '26', '27',
-            '28', '29', '30',
-            '31',
-        ],
-        'time': [
-            '00:00', '06:00', '12:00',
-            '18:00',
-        ],
-    },
-    '/project/projectdirs/dasrepo/ERA5/oct_2021_19_31_pl.nc')
+client = cdsapi.Client()
+client.retrieve(dataset, request).download()
