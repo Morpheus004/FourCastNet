1be56072d0e2c6dc356f55fbb6bd88c976358102
Morpheus004 <134723366+Morpheus004@users.noreply.github.com>
2025-01-21 22:31:34 +0530

    Made inference path appropriate and add print statements

--- inference/inference.py
+++ inference/inference.py
@@ -88,6 +88,7 @@ def load_model(model, params, checkpoint_file):
     model.zero_grad()
     checkpoint_fname = checkpoint_file
     checkpoint = torch.load(checkpoint_fname)
+    print("Printing contents of checkpoint ",dir(ch))
     try:
         new_state_dict = OrderedDict()
         for key, val in checkpoint['model_state'].items():
